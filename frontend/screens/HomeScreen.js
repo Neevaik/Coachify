239ed64bda42ed9584475702c2b452ebb5a3d137
Kevin <kevin.mavier@hotmail.com>
2024-03-12 17:10:14 +0100

    home page get fetch

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import { Text, NativeBaseProvider, Box, Stack, Slider } from "native-base";
-import { useState } from 'react';
+import { Text, NativeBaseProvider } from "native-base";
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { IPADDRESS, PORT } from '../ipaddress';
 
 export default function HomeScreen() {
+  const user = useSelector(state => state.user);
+  const [objectives, setObjectives] = useState([]);
+  const [error, setError] = useState(null);
 
-  const user = useSelector(state=> state.user)
-  
+  useEffect(() => {
+    const getObjectives = async () => {
+      try {
+        const response = await fetch(`http://${IPADDRESS}:${PORT}/objectives/getByUserId?user_id=${user.user_id}`);
+        if (!response.ok) {
+          throw new Error('Error please restart');
+        }
+        const data = await response.json();
+        setObjectives(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    getObjectives();
+  }, []);
 
   return (
     <NativeBaseProvider>
-      <Text>{user.name}</Text>
-      <Text>{user.email}</Text>
-      <Text>{user.password}</Text>
-      <Text>{user.gender}</Text>
-      <Text>{user.activity}</Text>
-      <Text>{user.height}</Text>
-      <Text>{user.birthdate}</Text>
+      {objectives.map((objective, index) => (
+        <Text key={index}>The goal is : {objective.objective}</Text>
+      ))}
+      {error && <Text>Error: {error}</Text>}
     </NativeBaseProvider>
   );
 }
